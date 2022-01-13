acce12686c472bf0a809113b000560509247b576
heyyayesh <biswalayesh3@gmail.com>
2022-01-13 20:34:47 +0530

    remove duplicate files

--- capitalize.test.js
+++
@@ -1,5 +0,0 @@
-import capitalize from './capitalize';
-
-test('capitalizes first letter', () => {
-    expect(capitalize('puppy')).toBe('Puppy');
-});
--- reverseString.test.js
+++
@@ -1,5 +0,0 @@
-import reverseString from './reverseString';
-
-test('reverses the string', () => {
-    expect(reverseString('supper with pupper')).toBe('reppup htiw reppus');
-});
